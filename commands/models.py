904d2c7d29829ace4a3f4843bc144fc2fdd784f6
prabhatsin <prabhat.singh112244@gmail.com>
2026-06-08 10:27:53 +0530

    models_command function completed

--- commands/models.py
+++ commands/models.py
@@ -2,42 +2,44 @@
 import click
 import json
 @click.command('models')
-# @click.option('-m', '--model', default='all', help='name of the model')
+@click.option('-p', '--provider', default='all', help='name of the model')
 #! Keerats boiler plate had .option wala part 
 #! Get clarity on when to use click.option and when not to 
-
-def models_command():
+def models_command(provider):
+    # This line handles the part when user throws provider name in
+    # small letters like google or anthropic instead of Google / Anthropic
+    provider=provider.capitalize()
     all_model_list=[]
     with open('models.json','r',encoding='utf-8') as file:
         object=json.load(file)
         providers_list=list(object["providers"].keys())
-        for provider in providers_list:
-            all_model_list.extend(object["providers"][provider]['models'])
-    click.echo(all_model_list)
+
+        if provider=='all':
+            for each_provider in providers_list:
+                all_model_list.extend(object["providers"][each_provider]['models'])
+            click.echo(all_model_list)
+        else:
+            if provider not in providers_list:
+                click.echo(f"Unknown provider :{provider}. Providers list is :{providers_list}")
+            else:
+                provider_model_list=object['providers'][provider]['models']
+                click.echo(provider_model_list)
 
 if __name__=='__main__':
+
+    print("This part only executes itself  when we directly run this script ")
     models_command()
 
 
-#?-----------------------------------------------------------------------
-# def models_command(provider):
-#     """Returns all the supported models"""
-#     with open('models.json','r',encoding='utf-8') as file:
-#         object=json.load(file)
-#         providers_list=list(object["providers"].keys())
-#         models_list=object["providers"][provider]['models']
-#     # print("Listing models...")
-#     # print({'model': models_list})
-#     click.echo(models_list)
+'''
+if we import the script (models.py) anywhere else the part below 'if __name__=='__main__':
+' does not execute becase then 
+__name__ is "myscript", not "__main__".
 
 '''
-# # '-m' --> Short flag 
-# # '--model' --> Long flag 
-# # default='all'  --> the value when user does not give the specific input 
-# The equivalent of 'return' and 'print' both in click framework is  cl
-
 '''
-#?---------------------------------------------------------------------------
+# click.echo — prints to the terminal. That's it. Function keeps running after it.
+'''
 
 
 
